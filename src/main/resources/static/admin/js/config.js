19352eec54f1b500e123594c93089e5824505355
lequochuy05 <lehuy2425@gmail.com>
2025-10-17 12:34:45 +0700

    add frontend for vercel

--- src/main/resources/static/admin/js/config.js
+++
@@ -1,5 +0,0 @@
-// src/main/resources/static/admin/js/config.js
-window.APP_BASE_URL = window.APP_BASE_URL || (location.origin.includes('10.50.252.193')
-  ? 'http://10.50.252.193:8080'
-  : location.origin.replace(/:\d+$/, '') + ':8080');
-  
