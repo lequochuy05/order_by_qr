069f2424cc2b459357f95b0752188386abee22ed
lequochuy05 <lehuy2425@gmail.com>
2025-10-21 00:14:05 +0700

    change onrender backend

--- frontend/admin/js/config.js
+++ frontend/admin/js/config.js
@@ -1,10 +1,6 @@
-// src/main/resources/static/admin/js/config.js
-// window.APP_BASE_URL = window.APP_BASE_URL || (location.origin.includes('10.50.252.193')
-//   ? 'http://10.50.252.193:8080'
-//   : location.origin.replace(/:\d+$/, '') + ':8080');
-  
+// src/main/resources/static/admin/js/config.js  
 window.APP_BASE_URL = window.APP_BASE_URL || (
   location.hostname.includes('vercel.app')
-  ? 'https://orderbyqr-production.up.railway.app'
+  ? 'https://order-by-qr.onrender.com'
   : 'http://localhost:8080'
 );
--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,10 +1,6 @@
 // src/main/resources/static/js/config.js
-// window.APP_BASE_URL = window.APP_BASE_URL || (location.origin.includes('localhost')
-//   ? 'http://localhost:8080'
-//   : location.origin.replace(/:\d+$/, '') + ':8080');
-
 window.APP_BASE_URL = window.APP_BASE_URL || (
   location.hostname.includes('vercel.app')
-  ? 'https://orderbyqr-production.up.railway.app'
+  ? 'https://order-by-qr.onrender.com'
   : 'http://localhost:8080'
 );
